c3b212855cb828bc72e20735f6567e40180bb980
Ing. Jad Zaza <jad.zaza@gmail.com>
2018-12-15 17:47:52 +0100

    mqtt

--- backend/controllers/mqtt.js
+++ backend/controllers/mqtt.js
@@ -1,6 +1,44 @@
 //test
 module.exports = (req, res) => {
-    return res.json({
-        message: 'test'
-    });
+
+  var mqtt=require('mqtt');
+
+  var client = mqtt.connect("mqtt://m20.cloudmqtt.com:16027",{
+    clientId:"backend01",
+    username:"yiqndlhw",
+    password:"YBpbuT-Di4Nw",
+    clean:true}
+  );
+
+
+  client.on("connect",function(){
+    console.log("connected");
+    //json = "{\"debug\":true}";
+
+    var o = {
+        animazione: 'base',
+        frequenza: 1,
+        ripetizione: 10,
+        colore: 0,
+        linea:1
+    };
+    client.publish("notabug/tree/setanimation", JSON.stringify(o) );
+
+    client.end();
+  });
+
+  /*
+  if (client.connected==true){
+    client.publish("testtopic", "test message");
+
+  }*/
+
+
+  console.log("chiamata eseguita ");
+
+  return res.json({
+      message: 'test1'
+  });
+
+
 }
