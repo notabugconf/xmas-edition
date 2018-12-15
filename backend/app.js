efdfe3d8e96925dc2cc376e1456d3832c2c41d3d
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2018-12-15 17:45:01 +0100

    First implementation of GET/POST Requests

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,14 @@ const port = process.env.PORT || 3000
 const express = require('express');
 const app = express();
 
-const get_animation = require('./controllers/get-animations');
+const get_animations = require('./controllers/get-animations');
+const publish_animations = require('./controllers/publish-animations');
 
+const bodyParser = require('body-parser');
 
-app.get('/api/animations/', get_animation);
+app.use(bodyParser.json())
+app.get('/api/animations/', get_animations);
+app.post('/api/animations/load', publish_animations);
 
 app.listen(port, () =>
     console.log(`Server is listening on port ${port}.`)
